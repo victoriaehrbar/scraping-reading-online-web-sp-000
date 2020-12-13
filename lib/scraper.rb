9a4d06ad644db8a7c8e0749f6191c2ab4b0074a1
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-13 20:44:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,6 @@ doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 courses.each do |course|
+binding.pry
   puts course.text.strip
-  byebug
 end
